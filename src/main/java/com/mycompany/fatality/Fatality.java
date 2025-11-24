fe49a1dc2d4dff0e25238bbca278ce115218a2f2
kokoju <j.bastidas.1@estudiantec.cr>
2025-11-24 11:44:53 -0600

    Primer Commit

---
+++ src/main/java/com/mycompany/fatality/Fatality.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fatality;
+
+/**
+ *
+ * @author kokoju
+ */
+public class Fatality {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
